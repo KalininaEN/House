6a188196bd0fbb8a9c41a4ee47dbd07fd2d8cfb4
Homework1 <kalinina_en@hotmail.com>
2023-11-14 00:13:04 +0300

    corrected name of object deleted variable

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -2,25 +2,22 @@ import java.util.*;
 
 public class House {
     public static void main(String[] args) {
-        Map<String, Integer> neighbors = new HashMap<>();
+        Map<String, Integer> neighbours = new HashMap<>();
 
-        neighbors.put("Иванов Иван Иванович", 65);
-        neighbors.put("Иванова Мария Павловна", 60);
-        neighbors.put("Иванов Михаил Иванович", 35);
-        neighbors.put("Петрова Анна Сергеевна", 30);
-        neighbors.put("Петров Владимир Дмитриевич", 3);
-        neighbors.put("Петров Дмитрий Анатольевич", 32);
-        neighbors.put("Сидоров Иван Петрович", 75);
-        neighbors.put("Сидорова Елена Михайловна", 65);
-        neighbors.put("Семенова Ольга Сергеевна", 17);
-        neighbors.put("Семенова Инна Андреевна", 45);
+        neighbours.put("Иванов Иван Иванович", 65);
+        neighbours.put("Иванова Мария Павловна", 60);
+        neighbours.put("Иванов Михаил Иванович", 35);
+        neighbours.put("Петрова Анна Сергеевна", 30);
+        neighbours.put("Петров Владимир Дмитриевич", 3);
+        neighbours.put("Петров Дмитрий Анатольевич", 32);
+        neighbours.put("Сидоров Иван Петрович", 75);
+        neighbours.put("Сидорова Елена Михайловна", 65);
+        neighbours.put("Семенова Ольга Сергеевна", 17);
+        neighbours.put("Семенова Инна Андреевна", 45);
 
         List<String> listOfAdult = new ArrayList<>();
 
-        for (Map.Entry<String, Integer> entry : neighbors.entrySet()) {
-            String key = entry.getKey();
-            Integer value = entry.getValue();
-
+        for (Map.Entry<String, Integer> entry : neighbours.entrySet()) {
             if (entry.getValue() >= 18) {
                 listOfAdult.add(entry.getKey());
             }
